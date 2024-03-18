ab5c9a432eee7bd28b45f7eb1835c8e4f47aaca3
unkmc <whoreson@cheerful.com>
2024-03-18 11:10:48 -0400

    Create configuration example file, revoke old key, etc

---
+++ openai/ConfigurationExample.java
@@ -0,0 +1,18 @@
+package openai;
+
+public class ConfigurationExample {
+	// Fill these out
+	public String apiKey = "apiKey";
+	public String assistantId = "assistantId";
+
+	public String assistantThreadsUri = "https://api.openai.com/v1/threads";
+	public String assistantThreadId;
+	public String assistantMessagesUri;
+	public String assistantRunsUri;
+
+	public void setThreadId(String threadId) {
+		assistantThreadId = threadId;
+		assistantMessagesUri = "https://api.openai.com/v1/threads/" + assistantThreadId + "/messages";
+		assistantRunsUri = "https://api.openai.com/v1/threads/" + assistantThreadId + "/runs";
+	}
+}
